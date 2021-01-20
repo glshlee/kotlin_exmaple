070ea4911c0593b0930f263b92a5ef20fafcfc8b
glshlee <glshlee@gmail.com>
2021-01-20 23:19:00 +0900

    add coroutine dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 }
