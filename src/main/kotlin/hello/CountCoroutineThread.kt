8424a18ac3471dd725a213e70f8f59ee4c19dd5b
glshlee <glshlee@gmail.com>
2021-01-21 22:55:25 +0900

    first koroutine

---
+++ src/main/kotlin/hello/CountCoroutineThread.kt
@@ -0,0 +1,27 @@
+package hello
+
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) = runBlocking {
+    println("${Thread.activeCount()} threads active at the start")
+    val time = measureTimeMillis {
+        createCoroutines(3)
+    }
+    println("${Thread.activeCount()} threads active at end")
+    println("Took $time ms")
+}
+
+suspend fun createCoroutines(amount: Int) {
+    val jobs = ArrayList<Job>()
+    for (i in 1..amount) {
+        jobs += GlobalScope.launch {
+            println("Started $i in ${Thread.currentThread().name}")
+            delay(1000)
+            println("Finished $i in ${Thread.currentThread().name}")
+        }
+    }
+    jobs.forEach {
+        it.join()
+    }
+}
