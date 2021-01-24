914a3ff24120c6ea758af0185dee467f259c6877
glshlee <glshlee@gmail.com>
2021-01-24 14:47:26 +0900

    Add AtomicOperations

---
+++ src/main/kotlin/hello/coroutine/AtomicOperations.kt
@@ -0,0 +1,21 @@
+package hello.coroutine
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+
+var counter = 0
+
+fun main(args: Array<String>) = runBlocking {
+    val workerA = asyncIncrement(2000)
+    val workerB = asyncIncrement(100)
+    workerA.await()
+    workerB.await()
+    print("counter [$counter]")
+}
+
+fun asyncIncrement(by: Int) = GlobalScope.async {
+    for (i in 0 until by) {
+        counter++
+    }
+}
